5d03f78b2da944985e29fe04fa1f97ec2ae3b958
Akassharjun Shanmugarajah <akassharjun@gmail.com>
2022-01-20 19:20:03 +0530

    feat: setup project structure (#1)
    * refactor: modify the .gitignore file
    
    * added logo
    
    * setup the project
    
    * added page title
    
    * added more characters, fixed card sizing for lg views, & added a tiny banner for new characters

---
+++ components/card/character-card.js
@@ -0,0 +1,91 @@
+class CharacterCard extends HTMLElement {
+  constructor() {
+    super();
+    this.innerHTML = this.render();
+  }
+
+  render() {
+    // css
+    const styles = /* css */ `
+        #char-card-${this.get("character-name", true)} {
+            background: url(${this.get("background-image")
+              .split(" ")
+              .join("-")});
+            background-position: center;  
+            background-size: cover;
+            background-repeat: no-repeat; 
+            border-radius:16px; 
+            filter: drop-shadow(4px 4px 10px rgba(0, 0, 0, 0.25));
+        }
+
+        #character-weapon-type{
+          text-shadow: rgba(0, 0, 0, 1) 5px 5px 8px;
+        }
+
+        #character-name {
+            text-shadow: rgba(0, 0, 0, 1) 5px 5px 8px;
+            line-height: 1;
+        }
+
+        @media only screen and (max-width: 450px){
+
+            #character-weapon-type {
+                font-size: 0.75rem;
+                line-height: 2;
+            }
+
+            #character-name {
+                font-size: 1.4rem;
+            }
+
+            br {
+              display: none;
+            }
+        }
+      `;
+
+    // html template
+    const html = /* html */ `
+        <style>${styles}</style>
+        <div class="bg-blue-400 flex flex-row justify-between items-end px-5 pt-5 relative h-32 overflow-hidden md:aspect-square md:w-full md:h-full" 
+        id="char-card-${this.get("character-name", true)}">
+                <div class="flex flex-col justify-between h-full">
+                    <img class="h-7 md:h-12 w-fit" src="${this.get("vision-image")}" />
+                    <div id="character-info" class="pb-5">
+                        <p id="character-weapon-type" class="text-white text-thin">${this.get("weapon-type")}</p>
+                        <h1 id="character-name" class="text-white">${this.get("character-name")}</h1>
+                    </div>
+                </div> 
+                <div class="absolute right-0 top-4 px-1 bg-red-600 py-1 ${this.get("new")}"><p class="text-white text-xs">NEW</p></div>
+                <img src="${this.get("character-image")}" class="absolute right-5 top-0 md:right-0 md:top-5 md:w-40 w-20" style="z-index:-1;" />
+        </div>
+      `;
+
+    return html;
+  }
+
+  get(attr, id) {
+    if (attr == "new"){
+      console.log(this.getAttribute(attr));
+      if (this.getAttribute(attr) == "undefined") {
+        return "hidden"
+      } else {
+        return ""
+      }
+    }
+
+    let name = this.getAttribute(attr)
+
+    if (attr == "character-name" && !id) {
+      if (name.length < 8) {
+        return name;
+      }
+
+      return name.split(" ").join(" <br>");
+    }
+
+    return name.split(" ").join("-");
+  }
+}
+
+window.customElements.define("char-card", CharacterCard);
---
+++ index.js
@@ -0,0 +1,185 @@
+let characters = [
+  {
+    id: "KGSE",
+    name: "Keqing",
+    weaponType: "Sword",
+    vision: "Electro",
+  },
+  {
+    id: "KKSA",
+    name: "Kaedehara Kazuha",
+    weaponType: "Sword",
+    vision: "Anemo",
+  },
+  {
+    id: "HTPP",
+    name: "Hu Tao",
+    weaponType: "Polearm",
+    vision: "Pyro",
+  },
+  {
+    id: "XGPP",
+    name: "Xiangling",
+    weaponType: "Polearm",
+    vision: "Pyro",
+  },
+  {
+    id: "ZIPG",
+    name: "Zhongli",
+    weaponType: "Polearm",
+    vision: "Geo",
+    new: 4,
+  },
+  {
+    id: "KSBE",
+    name: "Kujou Sara",
+    weaponType: "Bow",
+    vision: "Electro",
+  },
+  {
+    id: "TABH",
+    name: "Tartaglia",
+    weaponType: "Bow",
+    vision: "Hydro",
+  },
+  {
+    id: "XOPA",
+    name: "Xiao",
+    weaponType: "Polearm",
+    vision: "Anemo",
+    new: 2,
+  },
+  {
+    id: "DCCP",
+    name: "Diluc",
+    weaponType: "Claymore",
+    vision: "Pyro",
+  },
+  {
+    id: "AICG",
+    name: "Arataki Itto",
+    weaponType: "Claymore",
+    vision: "Geo",
+  },
+  {
+    id: "RSPE",
+    name: "Raiden Shogun",
+    weaponType: "Polearm",
+    vision: "Electro",
+  },
+  {
+    id: "NGCG",
+    name: "Ningguang",
+    weaponType: "Catalyst",
+    vision: "Geo",
+  },
+  {
+    id: "SKCH",
+    name: "Sangonomiya Kokomi",
+    weaponType: "Catalyst",
+    vision: "Hydro",
+  },
+  {
+    id: "AOSG",
+    name: "Albedo",
+    weaponType: "Sword",
+    vision: "Geo",
+  },
+  {
+    id: "KASC",
+    name: "Kamisato Ayaka",
+    weaponType: "Sword",
+    vision: "Cryo",
+  },
+  {
+    id: "DABC",
+    name: "Diona",
+    weaponType: "Bow",
+    vision: "Cryo",
+  },
+  {
+    id: "BTPS",
+    name: "Bennett",
+    weaponType: "Sword",
+    vision: "Pyro",
+  },
+  {
+    id: "SEPC",
+    name: "Shenhe",
+    weaponType: "Polearm",
+    vision: "Cryo",
+    new: 1,
+  },
+  {
+    id: "GUBC",
+    name: "Ganyu",
+    weaponType: "Bow",
+    vision: "Cryo",
+    new: 3,
+  },
+];
+
+const characterDb = new PouchDB("characters", { auto_compaction: true });
+
+$(document).on("ready", async () => {
+  characters.sort(function (a, b) {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  });
+
+  characters.sort(function (a, b) {
+    if (a.new == undefined) {
+        return 1;
+    } else {
+      if (a.new > b.new) {
+          return 1;
+      }  else {
+        return -1;
+      }
+    }
+  });
+
+  $("#char-grid-view").html(populateCharacterGrid(characters));
+});
+
+function populateCharacterGrid(chars) {
+  let charGrid = ``;
+
+  chars.forEach((char) => {
+    // if (char.new != undefined) {
+    //   charGrid =
+    //     `
+    //         <div class="lg:col-span-2 md:col-span-4 col-span-12 h-full">
+    //             ${generateCharacterCard(char)}
+    //         </div> 
+    //     ` + charGrid;
+    //   return;
+    // }
+
+    charGrid += `
+            <div class="lg:col-span-2 md:col-span-4 col-span-12 h-full">
+                ${generateCharacterCard(char)}
+            </div>
+        `;
+  });
+
+  return charGrid;
+}
+
+function generateCharacterCard(char) {
+  return `
+    <char-card 
+        character-name="${char.name}"
+        weapon-type="${char.weaponType}"
+        background-image="/assets/images/background/${char.vision.toLowerCase()}.png"
+        vision-image="/assets/images/visions/${char.vision.toLowerCase()}.png"
+        character-image="/assets/images/characters/${char.name.toLowerCase()}.png"
+        new=${char.new}
+    />
+    `;
+}
